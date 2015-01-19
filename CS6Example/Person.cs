36cf331ac9913ec15f7e3ad19eb4913cc1ef3b87
Casey Watson <casey.j.watson@gmail.com>
2015-01-19 13:46:43 -0600

    Initial commit.

---
+++ CS6Example/Person.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CS6Example
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public List<string> EmailAddresses { get; set; }
+    }
+}
